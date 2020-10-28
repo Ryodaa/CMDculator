dbdab897546cb14577958ed35624757554c3a671
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2020-10-28 21:07:41 +0100

    commented out

--- CMDculator/CMDculator/Program.cs
+++ CMDculator/CMDculator/Program.cs
@@ -10,6 +10,58 @@ namespace CMDculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the operator:");
+            string oper = Console.ReadLine();
+            Console.WriteLine("Please enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (oper == "+" || oper == "-" || oper == "*" || oper == "/")
+            {
+                Console.WriteLine("The result of " + num1 + " " + oper + " " + num2 + " is = " + BasicOp(num1, oper, num2));
+            }
+            else if (oper == "^")
+            {
+                Console.WriteLine("placeholder");
+            }
+            else
+            {
+                Console.WriteLine("Operator currently not supported");
+            }
+
+            Console.ReadLine();
         }
+
+        static double BasicOp(double num1, string oper, double num2)
+        {
+            double resultBasicOp = 0;
+
+            if (oper == "+")
+            {
+                resultBasicOp = num1 + num2;
+            }
+            else if (oper == "-")
+            {
+                resultBasicOp = num1 - num2;
+            }
+            else if (oper == "*")
+            {
+                resultBasicOp = num1 * num2;
+            }
+            else if (oper == "/")
+            {
+                resultBasicOp = num1 / num2;
+            }
+            return resultBasicOp;
+        }
+
+        /*static double powOp(double num1, double num2)
+        {
+            double powNum = 1;
+            double toPow = 0;
+
+            for (powNum <= toPow, powNum++)
+        }*/
     }
 }
