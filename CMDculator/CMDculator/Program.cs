83e636670dfe81f65a04421da331542b9af1e7d5
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2020-10-30 00:25:18 +0100

    various

--- CMDculator/CMDculator/Program.cs
+++ CMDculator/CMDculator/Program.cs
@@ -23,18 +23,19 @@ namespace CMDculator
 
                 while (true)
                 {
-                    Console.WriteLine("Possible operators are: + (plus), - (minus), * (multiply), / (divide), s (square root)");
-                    Console.WriteLine("                        ^ (exponent), is% (x of y = %) and %of (% of x = y)");
+                    Console.WriteLine("Possible operators are: + (plus),            - (minus),  ");
+                    Console.WriteLine("                        * (multiply),        / (divide), ");
+                    Console.WriteLine("                        ^ (exponent),        sq (square root) ");
+                    Console.WriteLine("                        is% (x of y = %)     %of (% of x = y)");
                     Console.WriteLine(" ");
-                    Console.WriteLine("Please enter your equation. (ex: \"3 + 4\" or \"20 %of 100\")");
+                    Console.WriteLine("Please enter your equation (ex: \"3 + 4\" or \"20 %of 100\"). Spaces are required!");
                     Char spaceChar = ' '; // Char defined for split function which is "space"
                     string input = Console.ReadLine(); // user input is being saved in variable "input"
                     string[] inputArray = input.Split(spaceChar); // Creates an array and sets it equal to the user input, applies split function to the user input and splits it by "spaces"
                     double num1 = Convert.ToDouble(inputArray[0]); // Converts array position 0 from a string to a double and puts it into a unique variable so the program can use it for the methods
                     string oper = inputArray[1]; // Since array position 1 is the operators and therefore was always supposed to be a string, there is no need for conversion as the array is already a string
-                    double num2 = Convert.ToDouble(inputArray[2]); // Same thing as array position 0
 
-                    if (oper == "s") // Method to get the square root
+                    if (oper == "sq") // Method to get the square root
                     {
                         Console.WriteLine("The square root of " + num1 + " is " + Math.Sqrt(num1));
                         Console.WriteLine(" ");
@@ -42,6 +43,8 @@ namespace CMDculator
                         Console.ReadLine();
                         continue; // So the code will not prompt for num2 since num2 is redundant for the square root method
                     }
+
+                    double num2 = Convert.ToDouble(inputArray[2]); // Same thing as array position 0
 
                     if (oper == "+" || oper == "-" || oper == "*" || oper == "/") // Prints out results for +, -, * and / 
                     {
@@ -69,12 +72,19 @@ namespace CMDculator
                     Console.ReadLine();
                 }
             }
-            catch (Exception e)
+            catch (FormatException)
             {
                 Console.WriteLine(" ");
-                Console.WriteLine(e);
+                Console.WriteLine("Format error! Correct format is: \" number *space* operator *space* number (ex: 1 + 1) \"");
                 Console.ReadLine();
-
+                return;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Format error! Correct format is: \" number *space* operator *space* number (ex: 1 + 1) \"");
+                Console.ReadLine();
+                return;
             }
 
             static double BasicOp(double num1, string oper, double num2) // Method for addition, subtraction, multiplication and division
